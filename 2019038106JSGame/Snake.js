d48cb20a5b305605c225687edc7f991f41320114
jehyeon <s85737@gmail.com>
2021-05-13 16:41:36 +0900

    게임리셋 추가, 뱀 이동기능 추가

--- 2019038106JSGame/Snake.js
+++ 2019038106JSGame/Snake.js
@@ -1,14 +1,18 @@
 class Snake {
-    snake;
+    parts;
     //초기 10으로 설
     snakeLength = 10;
     snakeColor = 'green';
     snakeBorder = 'black';
     context = document.getElementById("js game").getContext("2d");
+    //ms
+    moveSpeed = 200;
+    moveX = -10;
+    moveY = 0;
 
     constructor() {
         // 초기 뱀 모양 중앙에서 ㅁㅁㅁㅁ 로 시작
-        this.snake = [
+        this.parts = [
             {x: gridWidth/2, y:gridHeight/2},
             {x: gridWidth/2 + snakeSize, y:gridHeight/2},
             {x: gridWidth/2 + snakeSize*2, y:gridHeight/2},
@@ -26,8 +30,8 @@ class Snake {
     }
 
     setCanvasSnakePart(){
-        for(let i=0; i < snake.snake.length; i++){
-            let parts = snake.snake[i];
+        for(let i=0; i < snake.parts.length; i++){
+            let parts = snake.parts[i];
             context.fillStyle = this.snakeColor;
             context.strokeStyle = this.snakeBorder;
             context.fillRect(parts.x, parts.y, this.snakeLength, this.snakeLength);
@@ -35,9 +39,76 @@ class Snake {
         }
 
     }
+
     getContext(){
         return this.context;
     }
+
+    detectChangeDirection(){
+
+        window.onkeydown = (ev => {
+                switch (ev.keyCode) {
+                    case 37:
+                        //press left key
+                        if(this.moveX == 10) break;
+                        this.moveX = -10;
+                        this.moveY = 0;
+                        break;
+                    case 38:
+                        //press down key
+                        if(this.moveY == 10) break;
+                        this.moveX = 0;
+                        this.moveY = -10;
+                        break;
+                    case 39:
+                        //press right key
+                        if(this.moveX == -10) break;
+                        this.moveX = 10;
+                        this.moveY = 0;
+                        break;
+                    case 40:
+                        //down key
+                        if(this.moveY == -10) break;
+                        this.moveX = 0;
+                        this.moveY = 10;
+                        break;
+                    default:
+                        console.log("some key");
+                        break;
+                }
+            }
+        );
+
+    }
+
+    moveSnake(){
+
+
+
+        const newParts = {x : this.parts[0].x + this.moveX, y: this.parts[0].y + this.moveY};
+        this.parts.unshift(newParts);
+
+        if(isItemDestroy()){
+            itemCoordX = Math.round(Math.random() *100)*10%gridWidth;
+            itemCoordY = Math.round(Math.random() *100)*10%gridHeight;
+            getItem();
+            score += 100;
+            itemNumber++;
+        }else{
+            this.parts.pop();
+        }
+
+        this.setCanvasSnakePart();
+    }
+
+    setMoveSpeed(moveSpeed){
+        this.moveSpeed = moveSpeed;
+    }
+
+    getMoveSpeed(){
+        return this.moveSpeed;
+    }
+
 
 
 }
--- 2019038106JSGame/snakeGame.js
+++ 2019038106JSGame/snakeGame.js
@@ -13,9 +13,19 @@ const frame = 60;
 let gridHeight = canvas.width;
 let gridWidth = canvas.height;
 let snake;
+let itemCoordX = Math.round(Math.random() *100)*10%gridWidth;
+let itemCoordY = Math.round(Math.random() *100)*10%gridHeight;
+let score = 0;
+let level = 1;
+let timer = 50;
+let GameEnd = false;
+let itemNumber = 0;
+let intervalId;
 const gridSize = gridHeight * gridWidth;
 const canvasBackground = 'gray';
 const snakeSize = 10;
+
+
 
 
 /**
@@ -24,9 +34,25 @@ const snakeSize = 10;
 function gameInit(){
     let gridHeight = canvas.width;
     let gridWidth = canvas.height;
-    console.log("game init");
+    console.log("game init gridHeight : " + gridHeight + "grid Width" + gridWidth);
+    clearCanvas();
     snake = new Snake();
     snake.setCanvasSnakePart();
+    snake.detectChangeDirection();
+    clearInterval(intervalId);
+
+    intervalId = setInterval(()=>{
+
+        if(isGameEnd()){
+            gameEnd();
+            return;
+        }else{
+            clearCanvas();
+            getItem();
+            snake.moveSnake();
+        }
+
+    },snake.getMoveSpeed())
 }
 
 function clearCanvas() {
@@ -36,7 +62,40 @@ function clearCanvas() {
     context.fillRect(0, 0, gridWidth, gridHeight);
     context.strokeRect(0, 0, gridWidth, gridHeight);
 
+
 }
+
+function getItem() {
+
+    context.fillStyle = 'yellow';
+    context.strokeStyle = 'red';
+    context.fillRect(itemCoordX, itemCoordY, 5, 5);
+    context.fillRect(itemCoordX, itemCoordY, 5, 5);
+}
+
+function isItemDestroy() {
+    if (itemCoordX === snake.parts[0].x
+        && snake.parts[0].y === itemCoordY)
+        return true;
+    else return false;
+}
+
+function isGameEnd() {
+
+    // 자신의 몸에 닿으면 게임 끝!
+    for (let i = 1; i < snake.length; i++) {
+        if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true
+    }
+
+}
+
+function gameEnd() {
+    clearCanvas();
+    clearInterval(intervalId);
+}
+
+
+
 
 
 
